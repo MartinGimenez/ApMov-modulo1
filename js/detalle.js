dcc0fcce177988914e3f50b42a5ec4cec6b6c8fc
Martin G <martin.a.gimenez@gmail.com>
2021-10-12 02:02:37 -0300

    menu responsivo

--- js/detalle.js
+++ js/detalle.js
@@ -14,27 +14,23 @@ function getGET()
 		url: "https://www.googleapis.com/books/v1/volumes/" + getString ,
 		dataType: "json",
 		success: function(data) {
-			//libro = "<ul>";
 
 			libromas = ""
 
 			var infoLibroimg = "<div id='btn'> <img src='"+data.volumeInfo.imageLinks.thumbnail+"' width='120px'/>";
-            var infoLibrotitle = "<h3>" + data.volumeInfo.title + "</h3>";
+            var infoLibrotitle = "<br><h3>" + data.volumeInfo.title + "</h3>";
             var infoLibrosubtitle = "<h4>" + data.volumeInfo.subtitle + "</h4>";
             var infoLibroautor = "<h4>" + data.volumeInfo.authors + "</h4>";
             var infoLibroedit = "<h5>" + data.volumeInfo.publisher + "</h5>";
             var infoLibroyear = "<h5>" + data.volumeInfo.publishedDate + "</h5>";
             var infoLibrodes = "<br><p>" + data.volumeInfo.description + "</p>";
             var infoLibrobtnbuy = "<button ><a href=" + data.saleInfo.buyLink + " target='_blank'>Comprar</a></button>";
-            console.log(data.saleInfo)
+            var infoLibrobtn = "<button type='submit' value=''><a href='mailto:direccion@correo.com?&subject=Libro recomendado&body=Hola! Te comparto este libro, " + data.volumeInfo.title +  ", esta MUY BUENO!!'>Compartir</a></button>";
 
-                libromas += infoLibroimg+infoLibrotitle+infoLibrosubtitle+infoLibroautor+infoLibroedit+infoLibroyear+infoLibrobtnbuy+infoLibrodes;
-                
+            console.log(data);
 
-                
-
-			//libro += "</ul>";
-
+            libromas += infoLibroimg+infoLibrotitle+infoLibroautor+infoLibroedit+infoLibroyear+infoLibrobtnbuy+infoLibrobtn+infoLibrodes;
+ 
 			$("#resultado2").html(libromas);
 
 			}
--- js/main.js
+++ js/main.js
@@ -1,55 +1,49 @@
 function buscarLibros() {
 	var buscar = document.getElementById('buscar').value;
-	//var is = "";
 	var opcion = document.getElementById('opción').value;
+	console.log(buscar, opcion);
 
-	var arg_modal = "";
-	console.log(buscar)
+	if (buscar == "") {
+		alert("Ingrese un valor a buscar");
+		
+	} 
+	else {
 
-	$.ajax({
-		type: 'GET',
-		url: "https://www.googleapis.com/books/v1/volumes?q=" + opcion + buscar + "&maxResults=10" + "&key=AIzaSyDsFlFumcGvU5v_5ToYQEikrzzFiINDyxM",
-		dataType: "json",
-		success: function(data) {
-			//libro = "<ul>";
-			console.log(data)
-			libro = ""
-			libromas = ""
-			for (i = 0; i < data.items.length; i++) {
-				/*resultado.innerHTML += "<h4>" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "</h4>"
-				resultado.innerHTML += "<h4>" + data.items[i].volumeInfo.title + "</h4>"
-				resultado.innerHTML += "<h4>" + data.items[i].volumeInfo.authors + "</h4>"
-				resultado.innerHTML += "<h4>" + data.items[i].volumeInfo.title + "</h4>"*/
+		$.ajax({
+			type: 'GET',
+			url: "https://www.googleapis.com/books/v1/volumes?q=" + opcion + buscar + "&orderBy=relevance" + "&maxResults=10" + "&key=AIzaSyDsFlFumcGvU5v_5ToYQEikrzzFiINDyxM",
+			dataType: "json",
+			success: function(data) {
+				//libro = "<ul>";
+				//console.log(data)
+				libro = "";
+				infoLibroimg = "";
+				infoLibroMore = "";
 
-				//var infoLibroimg = "<div id='btn'> <img src="+data.items[i].volumeInfo.imageLinks.thumbnail+" alt='img' width='130px'/>";
-                var infoLibroimg = "<div id='btn'> <img src="+data.items[i].volumeInfo.imageLinks.thumbnail+" alt='img' width='130px'/>";
+				for (i = 0; i < data.items.length; i++) {
+					
+					if (data.items[i].volumeInfo.imageLinks.thumbnail === undefined) {
+						console.log(data.items[i].volumeInfo.imageLinks.thumbnail);
+						return "";						
+					} 
+					
+					else {
 
-                var infoLibrotitle = "<h3>" + data.items[i].volumeInfo.title + "</h3>";
-                var infoLibrosubtitle = "<h4>" + data.items[i].volumeInfo.subtitle + "</h4>";
-                var infoLibroautor = "<h4>" + data.items[i].volumeInfo.authors + "</h4>";
-                var infoLibroedit = "<h5>" + data.items[i].volumeInfo.publisher + "</h5>";
-                var infoLibroyear = "<h5>" + data.items[i].volumeInfo.publishedDate + "</h5>";
-                var infoLibrodes = "<br><p>" + data.items[i].volumeInfo.description + "</p>";
-                var infoLibroMore = "<button><a href=./pages/vermas.html?" + data.items[i].id + ">Más</a></h3></button></div>";
-                var infoLibrobtnbuy = "<button ><a href=" + data.items[i].volumeInfo.buyLink + ">Comprar</a></button>";
-                var infoLibrobtn = "<button type='submit' value=''><a href='mailto:direccion@correo.com?&subject=Libro&body=Hola zoquete. Te comparto este libro, " + infoLibrotitle +  ", esta MUY BUENO!!'>Compartir</a></button></div>";
-                
-                    //libro += infoLibroimg+infoLibrotitle+infoLibroautor+infoLibrobtn;
-                    libro += infoLibroimg + infoLibroMore;
-                    console.log(libro);
+						infoLibroimg = "<div id='btn'> <img src=" + data.items[i].volumeInfo.imageLinks.thumbnail + " alt='img' width='130px'/>";
 
-                    libromas += infoLibroimg+infoLibrotitle+infoLibrosubtitle+infoLibroautor+infoLibroedit+infoLibroyear+infoLibrodes+infoLibrobtnbuy;
-                    }
-
-                    
-
-				//libro += "</ul>";
-				$("#resultado").html(libro);
-				//$("#resultado2").html(libromas);
-
+						console.log(data.items[i].volumeInfo.imageLinks.thumbnail);
+						
+						infoLibroMore = "<button><a href=./pages/vermas.html?" + data.items[i].id + ">Más</a></h3></button></div>";
+	
+						libro += infoLibroimg + infoLibroMore;
+						//console.log(libro);
+		
+						$("#resultado").html(libro);
+					}
+				}		
 			}
-			
-	})
+		})	
+	}
 }
 
 $(document).ready(function(){
